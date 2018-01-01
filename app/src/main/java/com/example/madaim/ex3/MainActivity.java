afeb7522d261f044d5f02973cd9be3ea29c07805
Madaim <yarden2001@hotmail.com>
2018-01-01 18:01:57 +0200

    k

--- app/src/main/java/com/example/madaim/ex3/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex3/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.madaim.ex3;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
     public Button plus;
     public Button minus;
     public Button mul;
@@ -20,10 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ziki();
-    }
+        String num1 = first.getText().toString();
+        String num2 = seccond.getText().toString();
 
-    private void ziki() {
         plus=(Button)findViewById(R.id.btPlus);
         minus=(Button)findViewById(R.id.btMinus);
         div=(Button)findViewById(R.id.btDiv1);
@@ -35,9 +36,6 @@ public class MainActivity extends AppCompatActivity {
         minus.setOnClickListener((View.OnClickListener) this);
         div.setOnClickListener((View.OnClickListener) this);
         mul.setOnClickListener((View.OnClickListener) this);
-
-
-
     }
 
     public void onClick(View view){
@@ -64,10 +62,34 @@ public class MainActivity extends AppCompatActivity {
                     result.setText("cant devide!");
                 }
                 break;
+        }
+    }
 
 
+    private class myWatcher implements TextWatcher
+    {
 
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (first.getText().length()== 0||seccond.getText().length()==0){
+                plus.setEnabled(false);
+                minus.setEnabled(false);
+                div.setEnabled(false);
+                mul.setEnabled(false);
+            }
+            else {
+                plus.setEnabled(true);
+                minus.setEnabled(true);
+                div.setEnabled(true);
+                mul.setEnabled(true);
+            }
         }
 
+        @Override
+        public void afterTextChanged(Editable s) {}
     }
+
 }
